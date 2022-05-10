113d99b8676acc1d7a2d97a2b92298d843295e1d
Andrea Robles <cardavila13@gmail.com>
2022-05-10 13:37:41 +0200

    Fixing index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import DiscoverView from '@/views/DiscoverView.vue'
 import UploadView from '@/views/UploadView.vue'
-import CollectionsView from '@/views/Collections.vue'
+import CollectionsView from '@/views/CollectionsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
