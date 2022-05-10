c6aa0ed88c1e7a8b66ef2efe71abc9503d3be7c7
Andrea Robles <cardavila13@gmail.com>
2022-05-10 11:58:39 +0200

    Adding firsts files of the app structure

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,6 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
+  <main class="home-view">
+    <h1>Vista Home</h1>
+    <p>Aquí mirarás tus recetas y más</p>
   </main>
 </template>
---
+++ src/views/UploadView.vue
@@ -0,0 +1,6 @@
+<template>
+  <main class="upload-view">
+    <h1>Vista Subir Recetas</h1>
+    <p>Aquí subirás tus recetas</p>
+  </main>
+</template>
