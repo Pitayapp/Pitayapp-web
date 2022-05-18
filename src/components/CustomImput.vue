dead48cff7f3e9be6aae2ea7babe108623d84e74
Noelia Plasencia Diaz <noeliaplasencia92@gmail.com>
2022-05-18 23:41:57 +0100

    text boxes to add user information

---
+++ src/components/CustomImput.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  props: {
+    title: String,
+    text: String
+  }
+}
+</script>
+<template>
+  <div>
+    <label>{{ title }}</label>
+    <input :placeholder="[[ text ]]">
+  </div>
+</template>
+<style lang="scss" scoped>
+div {
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+
+  label {
+    font-weight: bold;
+    font-size: 1.2rem;
+    margin-bottom: 10px;
+  }
+
+  input {
+    height: 40px;
+    width: 100%;
+    border-radius: 15px;
+    padding-left: 15px;
+    border-color: #E5E5E5;
+  }
+}
+</style>
