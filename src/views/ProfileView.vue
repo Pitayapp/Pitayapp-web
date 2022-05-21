ee0ba6f0d04981b0ded02671131520d2118d14d0
Noelia Plasencia Diaz <noeliaplasencia92@gmail.com>
2022-05-21 21:17:51 +0100

    added save and settings buttons

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,9 +1,11 @@
 <script>
 import CustomInput from '@/components/CustomInput.vue'
+import ToggleButton from '@/components/ToggleButton.vue'
 
 export default {
   components: {
-    CustomInput
+    CustomInput,
+    ToggleButton
   },
   data() {
     return {
@@ -29,15 +31,73 @@ export default {
       <CustomInput :title="user" :text="userText"/>
       <CustomInput :title="email" :text="emailText"/>
     </div>
+    <div class="buttons-submit">
+      <button>Guardar cambios</button>
+      <p>+ Subir nueva receta</p>
+    </div>
+    <div class="configuration">
+      <h1>Configuración</h1>
+      <div class="checkbox-button">
+        <label>Permitir notificaciones</label>
+        <ToggleButton/>
+      </div>
+    </div>
   </main>
 </template>
 
 <style lang="scss" scoped>
-  .userInformationContainer{
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-template-rows: auto;
-    grid-gap: 20px;
-    max-width: fit-content;
+.userInformationContainer {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: auto;
+  grid-gap: 20px;
+  max-width: fit-content;
+  margin-bottom: 20px;
+}
+
+.buttons-submit {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  margin-bottom: 50px;
+
+  button {
+    background: #F9458E;
+    border-radius: 30px;
+    border-color: transparent;
+    font-size: 1rem;
+    font-weight: bold;
+    color: #FFFFFF;
+    width: 200px;
+    height: 40px;
+
+    &:hover {
+      background: #b83268;
+      cursor: pointer;
+    }
   }
+
+  p {
+    font-size: 1rem;
+    font-weight: bold;
+    color: #F9458E;
+
+    &:hover {
+      color: #b83268;
+      cursor: pointer;
+    }
+  }
+}
+
+.checkbox-button{
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin: 10px;
+
+  label {
+    font-size: 1.2rem;
+  }
+}
+
 </style>
