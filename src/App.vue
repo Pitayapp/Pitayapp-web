87ed881115261a09262c3eb1957dc5664d9ed629
iaguerri <ireneaguerri@gmail.com>
2022-05-27 16:57:53 +0200

    App vue correct

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 import { /*RouterLink,*/ RouterView /*useRoute*/ } from "vue-router";
 import SideBar from "@/components/SideBar.vue";
 import HeaderApp from "@/components/HeaderApp.vue";
-
 /* const route = useRoute();
 const path = route.path; */
 </script>
@@ -24,8 +23,7 @@ const path = route.path; */
 </template>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Spectral:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&family=Vollkorn:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-
+@import url("https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Spectral:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&family=Vollkorn:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
 :root {
   --primary: #f9458e;
   --secondary: #fff;
@@ -33,24 +31,24 @@ const path = route.path; */
   --sidebar-width: 310px;
   --dope: rgb(221, 220, 220, 0.1);
 }
-
 * {
-  font-family: 'Nunito', sans-serif;
+  font-family: "Nunito", sans-serif;
   background-color: transparent;
   box-sizing: border-box;
   margin: 0;
   padding: 0;
   transition: 0.5s ease-out;
-
+  body {
+    width: 100vw;
+    height: 100vh;
+    #app {
       width: 100vw;
       height: 100vh;
       margin: 0;
       padding: 0;
-
       /* @media (max-width: 800px) {
         //height: 140vh; //This allows to do vertical scroll in mobile version
       } */
-
       @media (max-width: 800px) {
         // height: 0vh; //This allows to do vertical scroll in mobile version
         a.active.router-link-exact-active {
@@ -58,21 +56,17 @@ const path = route.path; */
             saturate(7317%) hue-rotate(310deg) brightness(98%) contrast(99%);
         }
       }
-
       main {
         width: 100vw;
         height: 100%;
-
         @media (max-width: 800px) {
           height: 100vh;
         }
-
         .router-view {
           /* padding: 1.5rem 0 0 10rem; */
           /* box-sizing: border-box;
           width: 100vw;
           height: 100%; */
-
           @media (max-width: 800px) {
             padding: 0;
           }
@@ -81,25 +75,19 @@ const path = route.path; */
     }
   }
 }
-
 // route transitions
-
 .route-center-from {
   opacity: 0;
   transform: translateX(50px);
 }
-
 .route-enter-active {
   transition: all 0.4s ease-in;
 }
-
 .route-leave-to {
   opacity: 0;
   transform: translateX(-50px);
 }
-
 .route-leave-active {
   transition: all 0.4s ease-out;
 }
 </style>
-
