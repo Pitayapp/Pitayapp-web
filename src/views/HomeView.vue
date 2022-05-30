13c8631e4681a38cd4892f602faaf67ea357692b
Antonio Mateos <antoniomateosmariscal@gmail.com>
2022-05-30 19:03:01 +0200

    Fixing img in home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script>
-import croquetasTofuImage from "/src/assets/recipes/croquetas_tofu.jpg";
-import hamburguesaLentejasImage from "/src/assets/recipes/hamburguesa_lentejas_tofu.jpg";
-import clafoutisFrambuesaImage from "/src/assets/recipes/clafoutis-frambuesa.jpg";
+import croquetasTofuImage from "../assets/recipes/croquetas_tofu.jpg";
+import hamburguesaLentejasImage from "../assets/recipes/hamburguesa_lentejas_tofu.jpg";
+import clafoutisFrambuesaImage from "../assets/recipes/clafoutis-frambuesa.jpg";
 export default {
   name: "HomeView",
 
