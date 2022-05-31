faeff4fecd1cae07dee045707b5b6c7ac9b14c5f
Andrea Robles <cardavila13@gmail.com>
2022-05-31 17:40:07 +0200

    Creating Recipe Lists views

---
+++ src/views/SmoothiesList.vue
@@ -0,0 +1,20 @@
+<script setup>
+import SearcherBar from '@/components/SearcherBar.vue'
+</script>
+<template>
+  <main class="recipe-list-view">
+    <div class="recipe-list">
+      <div class="container">
+        <div class="top-discover">
+          <div class="section-title">
+            <h1>Descubre</h1>
+
+            <SearcherBar class="searcher-comp" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+<style lan="scss" scoped>
+</style>
